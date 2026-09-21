ad6d0ec5d8f9ac7c825e3594658208daa137a3c4
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 01:47:13 +0200

    test: cover Community+ Orbit SCEP keys

---
+++ orbit/pkg/communityplus/scep/scep_test.go
@@ -0,0 +1,35 @@
+package scep
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"testing"
+)
+
+func TestPublicKeysEqual(t *testing.T) {
+	first, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	second, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	equal, err := PublicKeysEqual(&first.PublicKey, &first.PublicKey)
+	if err != nil || !equal {
+		t.Fatalf("same key: equal=%v err=%v", equal, err)
+	}
+
+	equal, err = PublicKeysEqual(&first.PublicKey, &second.PublicKey)
+	if err != nil || equal {
+		t.Fatalf("different keys: equal=%v err=%v", equal, err)
+	}
+}
+
+func TestPublicKeysEqualRejectsNil(t *testing.T) {
+	if _, err := PublicKeysEqual(nil, nil); err == nil {
+		t.Fatal("expected nil public keys to be rejected")
+	}
+}
