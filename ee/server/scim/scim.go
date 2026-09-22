a392ade40e24bb4dfb28164a41e441243512d77e
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 15:16:47 +0200

    refactor: type Community+ SCIM compatibility facade

--- ee/server/scim/scim.go
+++ ee/server/scim/scim.go
@@ -1,14 +1,19 @@
-// Package scim is a compatibility facade for the Community+ SCIM boundary.
+// Package scim is a compatibility facade for the Community+ SCIM provider.
 package scim
 
 import (
+	"log/slog"
+	"net/http"
+
 	communityscim "github.com/fleetdm/fleet/v4/server/communityplus/scim"
+	"github.com/fleetdm/fleet/v4/server/config"
+	"github.com/fleetdm/fleet/v4/server/fleet"
 	kithttp "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
 )
 
-func RegisterSCIM(a, b, c, d, e any) error {
-	return communityscim.RegisterSCIM(a, b, c, d, e)
+func RegisterSCIM(root *http.ServeMux, ds fleet.Datastore, svc fleet.Service, logger *slog.Logger, cfg *config.FleetConfig) error {
+	return communityscim.RegisterSCIM(root, ds, svc, logger, cfg)
 }
 
 func RegisterValidationRoutes(r *mux.Router, opts []kithttp.ServerOption) {
