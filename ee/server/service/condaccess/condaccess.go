a5a689d677faeef5c55d1a14480f5bda0ad9d20b
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 17:00:16 +0200

    fix: type conditional access IdP compatibility wrapper

--- ee/server/service/condaccess/condaccess.go
+++ ee/server/service/condaccess/condaccess.go
@@ -10,12 +10,13 @@ import (
 	"github.com/fleetdm/fleet/v4/server/config"
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	scepdepot "github.com/fleetdm/fleet/v4/server/mdm/scep/depot"
+	"github.com/throttled/throttled/v2"
 )
 
 func RegisterSCEP(ctx context.Context, mux *http.ServeMux, storage scepdepot.Depot, ds fleet.Datastore, logger *slog.Logger, cfg *config.FleetConfig) error {
 	return communitycondaccess.RegisterSCEP(ctx, mux, storage, ds, logger, cfg)
 }
 
-func RegisterIdP(a, b, c, d, e any) error {
-	return communitycondaccess.RegisterIdP(a, b, c, d, e)
+func RegisterIdP(mux *http.ServeMux, ds fleet.Datastore, logger *slog.Logger, cfg *config.FleetConfig, store throttled.GCRAStore) error {
+	return communitycondaccess.RegisterIdP(mux, ds, logger, cfg, store)
 }
