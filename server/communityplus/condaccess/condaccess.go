806d40d6e609034a873030c6de214e8e44ffbaa8
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 17:00:03 +0200

    refactor: replace conditional access IdP stub

--- server/communityplus/condaccess/condaccess.go
+++ server/communityplus/condaccess/condaccess.go
@@ -1,7 +1,2 @@
 // Package condaccess implements Community+ conditional-access integrations.
 package condaccess
-
-// RegisterIdP remains the compatibility boundary for the Okta SAML IdP while
-// the Community+ implementation is being wired. Unlike the SCEP path, it does
-// not mount routes yet.
-func RegisterIdP(_ any, _ any, _ any, _ any, _ any) error { return nil }
