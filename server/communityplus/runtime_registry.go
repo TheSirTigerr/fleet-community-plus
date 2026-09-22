8306dbf9ff76e2d6cf5a24ceccb2401d01b7781e
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 17:52:48 +0200

    feat: mark conditional access available

--- server/communityplus/runtime_registry.go
+++ server/communityplus/runtime_registry.go
@@ -14,7 +14,7 @@ func newRuntimeRegistry() *Registry {
 	mustSetRuntimeStatus(registry, FeatureGitOps, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureSSO, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureSCIM, StatusAvailable)
-	mustSetRuntimeStatus(registry, FeatureConditionalAccess, StatusExperimental)
+	mustSetRuntimeStatus(registry, FeatureConditionalAccess, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureSoftwareAutomation, StatusExperimental)
 	mustSetRuntimeStatus(registry, FeaturePatchPolicies, StatusExperimental)
 	return registry
