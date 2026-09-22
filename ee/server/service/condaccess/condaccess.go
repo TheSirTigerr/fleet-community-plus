860193f32276d225020d3b6254660a966a0c32e4
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 16:55:07 +0200

    fix: type conditional access compatibility SCEP wrapper

--- ee/server/service/condaccess/condaccess.go
+++ ee/server/service/condaccess/condaccess.go
@@ -1,10 +1,19 @@
 // Package condaccess is a compatibility facade for Community+ conditional access.
 package condaccess
 
-import communitycondaccess "github.com/fleetdm/fleet/v4/server/communityplus/condaccess"
+import (
+	"context"
+	"log/slog"
+	"net/http"
 
-func RegisterSCEP(a, b, c, d, e, f any) error {
-	return communitycondaccess.RegisterSCEP(a, b, c, d, e, f)
+	communitycondaccess "github.com/fleetdm/fleet/v4/server/communityplus/condaccess"
+	"github.com/fleetdm/fleet/v4/server/config"
+	"github.com/fleetdm/fleet/v4/server/fleet"
+	scepdepot "github.com/fleetdm/fleet/v4/server/mdm/scep/depot"
+)
+
+func RegisterSCEP(ctx context.Context, mux *http.ServeMux, storage scepdepot.Depot, ds fleet.Datastore, logger *slog.Logger, cfg *config.FleetConfig) error {
+	return communitycondaccess.RegisterSCEP(ctx, mux, storage, ds, logger, cfg)
 }
 
 func RegisterIdP(a, b, c, d, e any) error {
