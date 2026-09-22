50e9c7f19161fbd5916f4bdbf30ff422ed1071b4
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 02:01:17 +0200

    test: cover default Community+ EST construction

--- server/communityplus/est/est_test.go
+++ server/communityplus/est/est_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
+func TestNewServiceWithoutOptions(t *testing.T) {
+	if svc := NewService(); svc == nil {
+		t.Fatal("expected default EST service")
+	}
+}
+
 func TestNewServiceAppliesOptions(t *testing.T) {
 	called := false
 	svc := NewService(
