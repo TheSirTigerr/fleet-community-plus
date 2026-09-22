448eb02244b668693132f37e9a94181a5759845c
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2026-09-22 15:32:48 +0200

    feat: mark Community+ SCIM available

--- server/communityplus/runtime_registry.go
+++ server/communityplus/runtime_registry.go
@@ -13,7 +13,7 @@ func newRuntimeRegistry() *Registry {
 	mustSetRuntimeStatus(registry, FeatureCustomTables, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureGitOps, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureSSO, StatusAvailable)
-	mustSetRuntimeStatus(registry, FeatureSCIM, StatusExperimental)
+	mustSetRuntimeStatus(registry, FeatureSCIM, StatusAvailable)
 	mustSetRuntimeStatus(registry, FeatureSoftwareAutomation, StatusExperimental)
 	mustSetRuntimeStatus(registry, FeaturePatchPolicies, StatusExperimental)
 	return registry
